4d0b22cac44f1378a8955944088653e4b804a0db
Pedro Tigre <pedrogtigre@gmail.com>
2023-05-26 16:29:41 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	prompt "example/focus-api/prompts"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+type RequestBody struct {
+	Topic string `json:"topic" binding:"required"`
+}
+
+type ResponseBody struct {
+	Phrases []string `json:"phrases"`
+}
+
+func generatePhrases(clientOpenAi *openai.Client, topic string) ([]string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	resp, err := clientOpenAi.CreateChatCompletion(
+		ctx,
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: prompt.SystemMessage(),
+				},
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: prompt.HumanMessage(topic),
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(resp.Choices) == 0 {
+		return nil, errors.New("no phrases generated")
+	}
+
+	var phrases []string
+	for _, choice := range resp.Choices {
+		phrases = append(phrases, choice.Message.Content)
+	}
+
+	return phrases, nil
+}
+
+func handleGeneratePhrases(clientOpenAi *openai.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var reqBody RequestBody
+		if err := c.ShouldBindJSON(&reqBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		phrases, err := generatePhrases(clientOpenAi, reqBody.Topic)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, ResponseBody{Phrases: phrases})
+	}
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("error loading .env file")
+		return
+	}
+
+	apiKey := os.Getenv("OPENAI_KEY")
+	clientOpenAi := openai.NewClient(apiKey)
+
+	router := gin.Default()
+	router.POST("/frases", handleGeneratePhrases(clientOpenAi))
+	router.Run(":8080")
+}
---
+++ prompts/prompts.go
@@ -0,0 +1,25 @@
+package prompts
+
+import "fmt"
+
+func SystemMessage() string {
+	return `Generator is a powerful language model designed to generate motivational phrases for a system role. With its advanced natural language processing capabilities, 
+	Generator can produce human-like text that inspires and motivates. Whether you need a boost of confidence or a push to achieve your goals, Generator is here to help. 
+	With its constantly evolving knowledge and ability to understand and process large amounts of text, Generator can provide personalized and relevant motivational phrases 
+	to help you reach your full potential. Generator can think in english, but all the responses should be in the user's language (the default language is portuguese).`
+}
+
+func HumanMessage(topic string) string {
+	return fmt.Sprintf(`Generator will generate a reponse based on the "USER'S INPUT", the response will follow the "RESPONSE FORMAT INSTRUCTIONS".
+
+	RESPONSE FORMAT INSTRUCTIONS
+	----------------------------
+	When responding to me, please output a response in this json format:
+	[{"phrase": "the phrase generated"}, {"phrase": "the second phrase generated"}, ...]
+
+	USER'S INPUT
+	----------------------------
+	Here is the user's input (remember to respond with a json array, and NOTHING else):
+	Generate 5 motivational phrases to make me leave my smartphone based on topic of %s. 
+	Each phrase should be returned as an json object following this structure: {"phrase": "the phrase generated"}.`, topic)
+}
